7af28ccf2f4e7b1d8b6ce716d9e754199f9deb9c
shu913 <sss91327@icloud.com>
2023-02-25 18:12:39 +0900

    Introduction components

---
+++ components/hero.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>CUBE</h1>
+      <p>Output Website</p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+// import React from "react";
+import Header from 'components/header'
+import Footer from 'components/footer'
+import Hero from 'components/hero'
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
+    </>
   )
 }
