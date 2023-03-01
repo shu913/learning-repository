9fb530aab5b5d319696257a28686a495c50666c7
shu913 <sss91327@icloud.com>
2023-03-01 11:32:39 +0900

    スタイルを適用

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,13 @@
+import styles from 'styles/hero.module.css'
+
 export default function Header({ title, subtitle, imageOn = false }) {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
       {imageOn && <figure> [画像] </figure>}
     </div>
-  )
+  );
 }
