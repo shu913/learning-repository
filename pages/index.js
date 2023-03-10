a9c6f6252581b529795350d619848d0ba3569e31
shu913 <sss91327@icloud.com>
2023-03-10 23:50:42 +0900

    テキストを編集

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
-// import React from "react";
-import Container from 'components/container'
-import Layout from 'components/layout'
-import Hero from 'components/hero'
-
 export default function Home() {
   return (
-    <Container>
-      <Hero
-        title="CUBE"
-        subtitle="Output Website"
-        imageOn
-      />
-    </Container>
+    <h1>
+      Shuji Watanabe
+    </h1>
   )
 }
